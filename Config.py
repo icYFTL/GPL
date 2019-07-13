ecf13f3bdfafee0b51dc5aaf8d8fb0a20aa57dc7
icYFTL <savap0@yandex.ru>
2019-07-14 02:23:45 +0300

    Config.py Update

--- Config.py
+++ Config.py
@@ -3,8 +3,10 @@ class Config:
     name = 'GPL'
     author = 'icYFTL'
     version = '1.4'
-    module_mod = False
 
-    # COULD BE CHANGED
+    # MAY BE CHANGED
     user_vk_access_token = ''
     save_results = True
+
+    # CAN BE CHANGED
+    module_mod = False
